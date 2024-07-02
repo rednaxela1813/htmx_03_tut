8f62b819173c56065d439a9a9c52a06eb63cda44
rednaxela1813 <rednaxela1813@gmail.com>
2024-07-02 18:10:57 +0200

    Models and base template added.

--- party/admin.py
+++ party/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Party, Gift, Guest, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ["venue", "party_date", "party_time", "organizer"]
+    search_fields = ["venue", "party_date", "party_time", "organizer"]
+    readonly_fields = ["uuid"]
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ["gift", "price", "link", "party"]
+    search_fields = ["gift", "price", "link", "party"]
+    readonly_fields = ["uuid"]
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ["name", "attending", "party"]
+    search_fields = ["name", "attending", "party"]
+    readonly_fields = ["uuid"]
--- party/models.py
+++ party/models.py
@@ -1,5 +1,51 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db import models
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Party(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    party_date = models.DateField()
+    party_time = models.TimeField()
+    invitation = models.TextField()
+    venue = models.CharField(max_length=255)
+    organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="organized_name")
+
+
+    class Meta:
+        verbose_name_plural = "parties"
+
+    def __str__(self):
+        return f"{self.venue} on {self.party_date}"
+    
+
+
+class Gift(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    gift = models.CharField(max_length=255)
+    price = models.FloatField(blank=True, null=True)
+    link = models.URLField(max_length=255, blank=True, null=True)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.gift
+    
+
+class Guest(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255)
+    attending = models.BooleanField(default=False)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE, related_name="guests")
+    
+
+    def __str__(self):
+        return str(self.name)
+    
+
+
